a4e0b7e47952154475aba4565ac9725317f78e69
Sean McGovern <sean.mcgovern@lob.com>
2022-02-02 15:06:46 -0800

    colorscheme update

--- init.lua
+++ init.lua
@@ -235,7 +235,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Enable the following language servers
-local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'hls', 'cssls', 'html', 'svelte', 'tailwindcss' }
+local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', 'typescript-language-server', 'hls', 'cssls', 'html', 'svelte', 'tailwindcss' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
